4e8105ddcd3e3dcaa53623a734a0d81916ddd075
Doki <duvictor514@gmail.com>
2020-02-06 11:01:04 -0800

    Create PythonPaint.py

---
+++ PythonPaint.py
@@ -0,0 +1,102 @@
+"""
+LESSON: TechSmart Studio Project
+"""
+
+#### ---- SETUP ---- ####
+
+# --- Libraries --- #
+
+# A1:
+import pygame
+pygame.init()
+import tsk
+
+WIDTH = 600
+HEIGHT = 600
+
+PALETTE = [(0,0,0),
+           (255,255,255),
+           (255,0,0),
+           (255,130,0),
+           (255,255,0),
+           (0,255,0),
+           (0,255,255),
+           (0,0,255),
+           (150,50,255),
+           (255,0,255),
+           (255,150,200),
+           (150,75,0)]
+           
+paletteDX = WIDTH / len(PALETTE)
+paletteDY = 30
+brushSize = 10
+drawColor = (0, 0, 0)
+polygonPoints = []
+
+w = pygame.display.set_mode([WIDTH, HEIGHT])
+
+def drawPal():
+    i = 0
+    while i < len(PALETTE):
+        rekt = pygame.Rect(int(i * paletteDX), 0, int(paletteDX), paletteDY)
+        pygame.draw.rect(w, PALETTE[i], rekt)
+        i += 1
+        
+def mouseInPal(my):
+    return my >= 0 and my <= paletteDY + brushSize
+
+def clearScreen():
+    w.fill((255, 255, 255))
+
+def drawCrossHair(mx, my):
+    invR = 0
+    invG = 0
+    invB = 0
+    pygame.draw.line(w, (invR, invG, invB), (mx - 2, my), (mx + 2, my), 1)
+    pygame.draw.line(w, (invR, invG, invB), (mx, my - 2), (mx, my + 2), 1)
+    pygame.display.flip()
+    
+running = True
+painting = False
+
+drawPal()
+
+while running:
+    mx, my = pygame.mouse.get_pos()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            break
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            painting = True
+        if event.type == pygame.MOUSEBUTTONUP:
+            painting = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                if brushSize >= 1:
+                    brushSize -= 1
+            elif event.key == pygame.K_UP:
+                if brushSize <= 60:
+                    brushSize += 1
+            elif event.key == pygame.K_m:
+                polygonPoints.append((mx, my))
+                drawCrossHair(mx, my)
+            elif event.key == pygame.K_p:
+                pygame.draw.polygon(w, drawColor, polygonPoints)
+                polygonPoints = []
+            elif event.key == pygame.K_SPACE:
+                clearScreen()
+                drawPal()
+                    
+    
+    if painting:
+        if mouseInPal(my):
+            palIdx = mx / paletteDX
+            drawColor = PALETTE[int(palIdx)]
+        else:
+            pygame.draw.circle(w, drawColor, (mx,my), brushSize)
+            
+    pygame.display.flip()
+
+
+# Turn in your Coding Project.
